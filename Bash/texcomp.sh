1f57331da5564ec2f04b1869e9b1b0e48e8afbe6
Evan Drake <evanmdrake@icloud.com>
2022-02-07 17:29:07 -0800

    created texcomp, optimized texcomp to take either a name= compile and open lsor compile all *.tex files in a dir"

---
+++ Bash/texcomp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# if I entered the name of a file
+if [[$1 != 0]]
+then
+	fname=$1
+	# compile to pdf and open it
+	pdflatex $fname".tex" && open $fname".pdf"
+fi
+
+# if there are no .tex dont enter the loop
+shopt -s nullglob
+for i in *.tex; do
+	pdflatex $i
+done
