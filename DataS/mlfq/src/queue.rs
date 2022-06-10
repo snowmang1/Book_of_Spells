cdce40c7317ceeaf56d0c823f43a9a3dfd52fec8
Evan Drake <evanmdrake@icloud.com>
2022-06-09 18:37:39 -0700

    optimized pop to use &mut self

--- DataS/mlfq/src/queue.rs
+++ DataS/mlfq/src/queue.rs
@@ -7,15 +7,14 @@ struct Node {
     key: u16,
 }
 
-#[allow(dead_code)]
 impl Node {
+    #![allow(dead_code)]
     fn new(key: u16) -> Self{
         let n = Node {next: None, key};
         n
     }
 }
 
-#[allow(dead_code)]
 #[derive(Clone, Default, PartialEq, Debug)]
 struct Queue {
     head: Option<Box<Node>>, //current node
@@ -32,11 +31,16 @@ impl Queue {
         // main does not own n after this function call
         let n = Node {next: self.head, key: n.key}; // n points to previous head
         self.head = Some(Box::new(n)); // current head is now n
+        self.size += 1;
         self
     }
-    fn pop(mut self) -> Self {
-        // temperarily returns and unwraps head
-        self.head = self.head.unwrap().next;
-        self
+    fn pop(&mut self) -> Result<Node, String> {
+        let popped = match self.head.to_owned() {
+            Some(x) => *x,
+            None => panic!("ERROR: POPPED EMPTY QUEUE")
+        };
+        self.head = popped.next.to_owned();
+        self.size -= 1;
+        Ok(popped)
     }
 }
--- DataS/mlfq/src/queue/tests.rs
+++ DataS/mlfq/src/queue/tests.rs
@@ -6,26 +6,26 @@ use super::*;
 fn init_node_test() {
     let n = Node {next: None, key: 0};
     let n_test: Node = Default::default();
-    assert_eq!(n, n_test);
+    assert_eq!(n, n_test)
 }
 #[test]
 fn init_queue_test() {
     let q = Queue {head: Some(Box::<Node>::new(Default::default())), size: 1};
     let n: Node = Default::default();
-    assert_eq!(*q.head.unwrap(), n);
+    assert_eq!(*q.head.unwrap(), n)
 }
 #[test]
 fn queue_default_test() {
     let q: Queue = Default::default();
     let q_test = Queue {head: None, size: 0};
-    assert_eq!(q, q_test);
+    assert_eq!(q, q_test)
 }
 
 #[test]
 fn queue_new_test() {
     let q = Queue::new();
     let q_test = Queue {head: None, size: 0};
-    assert_eq!(q, q_test);
+    assert_eq!(q, q_test)
 }
 #[test]
 fn push_simple_test() {
@@ -33,7 +33,7 @@ fn push_simple_test() {
     let n1 = Node {next: None, key: 1};
     let q = q.push(n1);
     let n_test = Node {next: None, key: 1};
-    assert_eq!(n_test, *q.head.unwrap());
+    assert_eq!(n_test, *q.head.unwrap())
 }
 #[test]
 fn push_two_test() {
@@ -43,30 +43,18 @@ fn push_two_test() {
     let n2 = Node {next: None, key: 2};
     let q = q.push(n2);
     let n_test = Node {next: Some(Box::new(Node{next: None, key: 1})), key: 2};
-    assert_eq!(n_test, *q.head.unwrap());
+    assert_eq!(n_test, *q.head.unwrap())
 }
 
 #[test]
 fn pop_simple_test() {
-    let q = Queue::new();
+    let mut q = Queue::new();
     let n1 = Node {next: None, key: 1};
-    let q = q.push(n1);
+    q = q.push(n1);
     let n2 = Node {next: None, key: 2};
-    let q = q.push(n2);
-    let q = q.pop();
-    let n_test = Node {next: None, key: 1};
-    assert_eq!(n_test, *q.head.unwrap());
-}
-#[test]
-fn pop_two_test() {
-    let q = Queue::new();
-    let n1 = Node {next: None, key: 1};
-    let q = q.push(n1);
-    let n2 = Node {next: None, key: 2};
-    let q = q.push(n2);
-    let n3 = Node {next: None, key: 3};
-    let q = q.push(n3);
-    let q = q.pop();
+    q = q.push(n2);
+    let q_test = q.pop();
     let n_test = Node {next: Some(Box::new(Node{next: None, key: 1})), key: 2};
-    assert_eq!(n_test, *q.head.unwrap());
+    assert_eq!(n_test, q_test.unwrap());
+    assert_eq!(q.size, 1);
 }
